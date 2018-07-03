c8648de0d9ff29901efb3416d517ca16a6a6ba5e
iamrajajamal <martian.hacked@gmail.com>
2018-07-03 20:38:19 +0500

    Create portfolio page

---
+++ src/views/PortfolioOne.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home main-content">
+    <div class="left-content">
+      <h1>Crypto LOGO</h1>
+      <p class="subtxt">Description blah blah blah</p>
+      <p class="subtxt">Skills and Experience blah blah blah</p>
+    </div>
+    <div class="right-content img1">
+      <button> <router-link to="/portfolio/one">Browse Work</router-link> </button>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style scoped>
+.img1 {
+  background: url('../assets/home.jpg');
+}
+</style>
